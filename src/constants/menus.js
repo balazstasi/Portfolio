f658414533679da9af98dbb03c608965495d3fa5
Tasi Balázs <balazstasi2@gmail.com>
2021-04-28 04:18:26 +0200

    Adjusted fonts

--- src/constants/menus.js
+++ src/constants/menus.js
@@ -1,6 +1,6 @@
 export const menuItems = [
-  { name: "Skills", icon: "fas fa-laptop-code" },
+  { name: "Skills | Projects", icon: "fas fa-laptop-code" },
   { name: "Education", icon: "fas fa-graduation-cap" },
   { name: "Workplaces", icon: "fas fa-briefcase" },
-  { name: "Hobbies, motivations", icon: "fas fa-gamepad" },
+  { name: "Hobbies | Motivations", icon: "fas fa-gamepad" },
 ];
